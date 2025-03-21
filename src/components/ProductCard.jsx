7bec6efcab8674b88714aa070c28173b27d5a293
Ibrahim Ashraf <xxibrahimashrafxx@gmail.com>
2025-03-21 21:10:40 +0200

    Edit some styles

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,10 +4,10 @@ function ProductCard({ product }) {
   const { id, title, price, image, rating } = product;
 
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-full max-w-sm transition-all duration-500 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:transform hover:scale-[1.02]">
       <Link to={`/product/${id}`}>
         <img
-          className="p-8 rounded-t-lg w-full h-[350px]"
+          className="w-full h-[350px] rounded-t-lg p-[20px] mb-[20px] bg-white"
           src={image}
           alt="product image"
         />
@@ -25,7 +25,9 @@ function ProductCard({ product }) {
           </span>
         </div>
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+            ${price}
+          </span>
           <Link
             to={`/product/${id}`}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -67,7 +67,7 @@ function Products() {
               </button>
             ))}
           </div>
-          <div className="grid lg:grid-cols-3 gap-[20px] px-[50px] py-[20px]">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-[20px] px-[50px] py-[20px]">
             {products.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
