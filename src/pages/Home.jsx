c28efe5f3aac7db14fb73fa199844d9b5bc8d19a
Ibrahim Ashraf <xxibrahimashrafxx@gmail.com>
2025-03-21 20:28:51 +0200

    Edit some colors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="min-h-screen dark:bg-gray-800">
       <Nav />
-      <h1 className="capitalize text-center my-[20px] text-4xl md:text-6xl font-bold text-red-900 dark:text-blue-800">
+      <h1 className="capitalize text-center my-[20px] text-4xl md:text-6xl font-bold text-red-900 dark:text-yellow-400">
         our products
       </h1>
       <Products />
